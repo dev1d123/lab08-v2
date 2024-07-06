f28f9c8bfa27ec0e89bf023ca4564968a8dcb8d8
dev1d123 <dhuamanio@unsa.edu.pe>
2024-07-06 12:55:35 -0500

    Implementacion de un svg para mostrar las imagenes, se procede a realizar una prueba mostrando todas las componentes

--- juego-hangman/src/app/app.component.ts
+++ juego-hangman/src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HangmanComponent } from './components/hangman/hangman.component';
+import { DisplayComponent } from './components/display/display.component';
+import { QuestionComponent } from './components/question/question.component';
+import { KeyboardComponent } from './components/keyboard/keyboard.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HangmanComponent, DisplayComponent, QuestionComponent, KeyboardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
